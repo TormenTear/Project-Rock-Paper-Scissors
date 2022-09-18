ca002c0df7fba0f20af6d22ea00ff6c54e831c4c
TormenTear <sindie_danyel@yahoo.com>
2022-09-18 12:44:55 +0200

    5 round game done & display winner. 1st part done!

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let compScore = 0;
 
 function getComputerChoice(){
   let random = Math.random();
-  console.log(random);
   if (random <= 0.33){
     return "rock";
   }
@@ -27,30 +26,55 @@ function playRound(playerSelection, compSelection){
     return "You tied! Scissors is equal scissors!";
   }
   else if (playerSelection === "rock" && compSelection === "scissors"){
+    playerScore++;
     return "You won! Rock smashes scissors!";
   }
+  
   else if (playerSelection === "rock" && compSelection === "paper"){
+    compScore++;
     return "You lost! Paper covers rock!";
   }
   else if (playerSelection === "paper" && compSelection === "scissors"){
+    compScore++;
     return "You lost! Scissors cuts paper";
   }
   else if (playerSelection === "paper" && compSelection === "rock"){
+    playerScore++;
     return "You won! Paper covers rock!";
   }
   else if (playerSelection === "scissors" && compSelection === "paper"){
+    playerScore++;
     return "You won! Scissors cuts paper!";
   }
   else (playerSelection === "scissors" && compSelection === "rock")
+    compScore++;
     return "You lost! Rock smashes scissors!";
   
 }
 
 function game(){
   for (let i = 0; i < 5; i++) {
-    
+    let playerSelection = prompt("Choose a weapon: ", "rock, paper, scissors").toLowerCase();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
   }
 }
+
+game();
+
+function checkWinner(){
+  if(playerScore < compScore){
+  return "You lost! Computer smashed you! Practice more!";
+  }
+  else if(playerScore > compScore){
+  return "Good game! You Won!";
+  }
+  else {
+    return "You tied!";
+}
+}
+
+console.log(checkWinner());
 
 
 //let playerSelection = prompt("Choose a weapon: ", "rock, paper, scissors").toLowerCase();
